823d25cfc092e23c88746b462d87b993310a7657
Shilpa Sathya Narayana <121907655+shilpasathyanarayana@users.noreply.github.com>
2023-05-03 10:27:35 -0600

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,15 @@
+/*
+ * How to begin a java program
+ * 
+ * write a public class any name
+ * save it in a file of the same name as the classname
+ * write public static void main(String[] args) method. write your code with this main method 
+ * 
+ */
+public class Application {
+
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+	}
+
+}
